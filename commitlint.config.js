a5d636bf5738bdb4939881b103fbbdff9d75184d
Super Snager <supersnager@gmail.com>
2020-11-07 16:37:54 +0100

    chore(commitlint): added rule for ignoring work in progress commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => message.indexOf("WIP:") === 0],
+};
